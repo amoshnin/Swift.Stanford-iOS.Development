1d98a834b32e7e30ea640d0bd7cb598bf3403e39
Artem Moshnin <tap.kap.tap@gmail.com>
2020-11-08 23:38:46 +0100

    refactoring

--- StanfordCourse/Views/Home.swift
+++ StanfordCourse/Views/Home.swift
@@ -29,14 +29,23 @@ struct CardView: View {
         GeometryReader { geometry in
             ZStack {
                 if card.isFaceUp {
-                    RoundedRectangle(cornerRadius: 10).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 3)
+                    RoundedRectangle(cornerRadius: CORNER_RADIUS).fill(Color.white)
+                    RoundedRectangle(cornerRadius: CORNER_RADIUS).stroke(lineWidth: STROKE_LINE_WIDTH)
                     Text(card.content)
                 } else  {
-                    RoundedRectangle(cornerRadius: 10).fill()
+                    RoundedRectangle(cornerRadius: CORNER_RADIUS).fill()
                 }
             }
-            .font(.system(size: min(geometry.size.width, geometry.size.height) * 0.70))
+            .font(.system(size: fontSize(size: geometry.size) ))
         }
     }
+    
+    // MARK: - Drawing Constants (Styling)
+    let CORNER_RADIUS: CGFloat = 10
+    let STROKE_LINE_WIDTH: CGFloat = 3
+    
+    func fontSize(size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.70
+    }
 }
+
