f05fccd1b6e1f1b43fae06023878fc215b841a25
Artem Moshnin <tap.kap.tap@gmail.com>
2020-11-10 14:46:45 +0100

    done

--- EmojiArt/EmojiArt/Views/Components/PaletteChooser.swift
+++ EmojiArt/EmojiArt/Views/Components/PaletteChooser.swift
@@ -4,7 +4,7 @@ struct PaletteChooser: View {
     @ObservedObject var document: DocumentViewModel
     @Binding var ChosenPalette: String
     
-    @State private var editPopupVisible = false
+    @State private var PopupVisible = false
     
     var body: some View {
         HStack {
@@ -15,9 +15,9 @@ struct PaletteChooser: View {
             Text(document.paletteNames[ChosenPalette] ?? "")
             Image(systemName: "keyboard")
                 .imageScale(.large)
-                .onTapGesture {  editPopupVisible = true }
-                .popover(isPresented: $editPopupVisible, content: {
-                    PaletteEditor(ChosenPalette: $ChosenPalette)
+                .onTapGesture {  PopupVisible = true }
+                .sheet(isPresented: $PopupVisible, content: {
+                    PaletteEditor(ChosenPalette: $ChosenPalette, PopupVisible: $PopupVisible)
                         .environmentObject(document)
                         .frame(minWidth: 300, minHeight: 500 )
                 })
@@ -29,26 +29,57 @@ struct PaletteChooser: View {
 struct PaletteEditor: View {
     @EnvironmentObject var document: DocumentViewModel
     @Binding var ChosenPalette: String
+    @Binding var PopupVisible: Bool
     
     @State private var palleteName: String = ""
-    
-    
-    
-    
+    @State private var newEmojis: String = ""
     
     var body: some View  {
         VStack (spacing: 0) {
-            Text("Palette editor")
-                .font(.headline)
-                .padding()
+            ZStack {
+                Text("Palette editor")
+                    .font(.headline)
+                    .padding()
+                
+                HStack {
+                    Spacer()
+                    Button(action: {PopupVisible = false}, label: {Text("Done")})
+                        .padding()
+                }
+            }
             
             Divider()
-            TextField("Palette Name", text: $palleteName)
-                .padding()
-            
-            Spacer()
+            Form {
+                Section {
+                    TextField("Palette Name", text: $palleteName, onEditingChanged: { began in
+                        if !began {document.renamePalette(ChosenPalette, to: palleteName)}
+                    })
+                    
+                    TextField("Add emoji", text: $newEmojis, onEditingChanged: { began in
+                        if !began {
+                            ChosenPalette = document.addEmoji(newEmojis, toPalette: ChosenPalette)
+                            newEmojis = ""
+                        }
+                    })
+                }
+                
+                Section (header: Text("Remove emojis")) {
+                    LazyVGrid (columns: [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]) {
+                        ForEach(ChosenPalette.map { String($0) }, id: \.self) {emoji in
+                            Text(emoji)
+                                .font(.system(size: EMOJI_SIZE))
+                                .padding(.vertical, 6)
+                                .onTapGesture { ChosenPalette = document.removeEmoji(emoji, fromPalette: ChosenPalette)}
+                            
+                        }
+                    }
+                    .padding()
+                }
+            }
         }
         .onAppear {palleteName = document.paletteNames[ChosenPalette] ?? ""}
     }
+    
+    private let EMOJI_SIZE: CGFloat = 30
 }
 
